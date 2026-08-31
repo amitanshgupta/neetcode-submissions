33d6270ced113ad3730d27fe01f859d9ed4d1d53
amitanshgupta <amitanshg1@gmail.com>
2026-08-31 20:39:24 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,70 @@
+class PrefixTree {
+public:
+    struct Node{
+        bool isEndOfWord;
+        Node* links[26];
+        
+        bool containsKey(char ch){
+            return (links[ch-'a']!=NULL);
+        }
+        void put(char ch, Node* node){
+            links[ch-'a'] = node;
+        }
+        Node* get(char ch){
+            return links[ch-'a'];
+        }
+        void setEnd(){
+            isEndOfWord = true;
+        }
+        bool isEnd(){
+            return isEndOfWord;
+        }
+    };
+
+    Node* root;
+
+    PrefixTree() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* node = root;
+        for(int i = 0; i< word.size(); i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        for(int i = 0; i<word.size(); i++){
+            if(!node->containsKey(word[i])){
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for(int i = 0; i<prefix.size(); i++){
+            if(!node->containsKey(prefix[i])){
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
